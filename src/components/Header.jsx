203468efaf943693f40ded8a2c731bb30ac2e453
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2024-02-15 20:40:48 +0530

    create sign in, sign up and forgot password pages and oauth component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,20 +23,23 @@ export default function Header() {
           <div>
             <ul className="flex space-x-10">
               
-              <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`}
-              onClick={()=>navigate("/")}
-              >
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold ${pathMathRoute("/") ? "text-black border-b-2 border-b-red-500" : "text-gray-400 border-b-transparent"
+              }`}
+              onClick={() => navigate("/")}
+            >
                 Home
               </li>
               
-              <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-              ${pathMathRoute("/offers") && "text-black border-b-red-500"}`}
+              <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMathRoute("/offers") ? "text-black border-b-2 border-b-red-500" : "text-gray-400 border-b-transparent"
+              }`}
               onClick={()=>navigate("/offers")}
               >
                 Offers
               </li>
               
-              <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}
+              <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMathRoute("/sign-in") ? "text-black border-b-2 border-b-red-500" : "text-gray-400 border-b-transparent"
+              }`}
               onClick={()=>navigate("/sign-in")}
               >
                 Sign In
