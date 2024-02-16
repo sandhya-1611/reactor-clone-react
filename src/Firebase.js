f1b8167e1f64cb08abdb6900ab08dafd20cd9821
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2024-02-16 15:31:28 +0530

    install firebase react-toastify and sign up the user

---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7GJ09z6Zu_qV64BXEpDauOWOEYGKAVLw",
+  authDomain: "realtor-clone-react-4d6e8.firebaseapp.com",
+  projectId: "realtor-clone-react-4d6e8",
+  storageBucket: "realtor-clone-react-4d6e8.appspot.com",
+  messagingSenderId: "739391899041",
+  appId: "1:739391899041:web:a3f071c1b270195656f7e0"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,11 @@ import {useState} from "react";
 import {AiFillEyeInvisible, AiFillEye} from "react-icons/ai"
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import {getAuth,createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {db} from "../Firebase";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function SignUp() {
 
@@ -15,15 +20,40 @@ export default function SignUp() {
   });
 
   const {name,email,password} = formData;
+  const navigate = useNavigate();
   function onChange(e)
   {
     setFormData((prevState)=>({
       ...prevState,
       [e.target.id]:e.target.value,
     }) )
-
-
   }
+
+  async function onSubmit(e){
+      e.preventDefault()
+
+  try {
+    const auth = getAuth()
+    const userCredentials = await createUserWithEmailAndPassword(auth,
+      email,
+      password
+      );
+
+    updateProfile(auth.currentUser,{
+      displayName: name
+    })
+    const user = userCredentials.user
+    const formDataCopy = {...formData};
+    delete formDataCopy.password;
+    formDataCopy.timestamp = serverTimestamp();
+
+    await setDoc(doc(db, "users", user.uid), formDataCopy)
+    // toast.success("Sign up was successfull")
+    navigate("/");
+  } catch (error) {
+    toast.error("Something went wrong with the registration")
+  }
+}
     return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign Up</h1>
@@ -34,7 +64,7 @@ export default function SignUp() {
         />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%]lg:ml-20">
-          <form >
+          <form onSubmit={onSubmit}>
           <input type="text" id="name" 
             value={name} 
             onChange={onChange}
