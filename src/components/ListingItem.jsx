50b3857f11b316453fe736fd3cfa04d9dfde480f
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2024-03-02 11:34:06 +0530

    add delete and edit functionality to the my listing section

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,7 +1,9 @@
 import Moment from "react-moment";
 import {Link} from "react-router-dom";
 import {MdLocationOn} from "react-icons/md"
-export default function ListingItem({listing,id}) {
+import { FaTrash } from "react-icons/fa6";
+import { MdEdit } from "react-icons/md";
+export default function ListingItem({listing,id, onEdit, onDelete}) {
   return (<li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m=[10px]">
     <Link classname="contents" to={`/category/${listing.type}/${id}`}>
       <img className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
@@ -42,6 +44,27 @@ export default function ListingItem({listing,id}) {
         </div>
       </div>
     </Link>
+    {onDelete && (
+      <FaTrash className="absolute 
+      bottom-2 
+      right-2 
+      h-[14px] 
+      cursor-pointer
+      text-red-500"
+      onClick={()=>onDelete
+        (listing.id)}
+      />
+    )}
+    {onEdit && (
+      <MdEdit className="absolute 
+      bottom-2 
+      right-7
+      h-4
+      cursor-pointer"
+      onClick={()=>onEdit
+        (listing.id)}
+      />
+    )} 
   </li>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { getAuth, updateProfile } from "firebase/auth";
-import { collection, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { db } from "../Firebase";
@@ -73,6 +73,21 @@ export default function Profile() {
     }
     fetchUserListings();
   },[auth.currentUser.uid]);
+  async function onDelete(listingID)
+  {
+    if(window.confirm("Are you sure you want to delete?"))
+    {
+      await deleteDoc(doc(db , "listings", listingID));
+      const updatedListings = listings.filter((listing)=> listing.id !== listingID
+      );
+      setListings(updatedListings)
+      toast.success("Successfully deleted the listing")
+    }
+  }
+  function onEdit(listingID)
+  {
+    navigate(`/edit-listing/${listingID}`)
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -125,7 +140,10 @@ export default function Profile() {
                 <ListingItem
                   key={listing.id}
                   id={listing.id}
-                  listing={listing.data}/>
+                  listing={listing.data}
+                  onDelete={()=>onDelete(listing.id)}
+                  onEdit={()=>onEdit(listing.id)}
+                  />
               ))}
             </ul>
           </>
