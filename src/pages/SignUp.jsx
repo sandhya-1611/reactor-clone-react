b144ea8755f8c7e21ea277538e28137f3a65b1ad
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2024-07-10 19:29:13 +0530

    sign up corrections

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -48,8 +48,8 @@ export default function SignUp() {
     formDataCopy.timestamp = serverTimestamp();
 
     await setDoc(doc(db, "users", user.uid), formDataCopy)
-    // toast.success("Sign up was successfull")
-    // navigate("/");
+    toast.success("Sign up was successfull")
+    navigate("/");
   } catch (error) {
     toast.error("Something went wrong with the registration")
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -7,5 +6,5 @@ module.exports = {
     extend: {},
   },
   plugins: [require('@tailwindcss/forms')],
-}
+};
 
